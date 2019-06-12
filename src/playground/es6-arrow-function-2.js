3dd55f77a151fb5c1097c4a25331cf9933410036
Ben <benclarkphoto@mac.com>
2019-06-12 00:01:09 -0700

    more arrow functions

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,42 +1,33 @@
-'use strict';
-
 // arguments objecgt - no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = (a, b) => {
   // console.log(arguments);
   return a + b;
 };
 console.log(add(55, 2, 445));
 
+
 // this keyword - no longer bound
 
-var user = {
+const user = {
   name: 'Benjamin',
   cities: ['Huntington Beach', 'California', 'Long Beach'],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    });
+  printPlacesLived() {
+    return this.cities.map((city) => this.name + ' has lived in ' + city);
   }
 };
 console.log(user.printPlacesLived());
 
 // Challenge area
 
-var multiplier = {
+const multiplier = {
   // numbers - array of numbers
   numbers: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55],
   // multiplyBy - single number
   multiplyBy: 5,
   // multiply - return a new array where the numbers have been multiplied
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (number) {
-      return _this2.multiplyBy * number;
-    });
+  multiply() {
+    return this.numbers.map((number) => this.multiplyBy * number);
   }
 };
 
