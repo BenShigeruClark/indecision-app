305d4deb4799470e7ef63ade502d8e1c8310e0dc
Ben <benclarkphoto@mac.com>
2019-06-18 22:45:14 -0700

    Component Props

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 class IndecisionApp extends React.Component {
   render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer';
+    const options = ['Thing one', 'Thing two', 'Thing four'];
+
     return (
       <div>
-        <Header />
+        <Header title={title} subtitle={subtitle} />
         <Action />
-        <Options />
+        <Options options={options}/>
+        <Option />
         <AddOption />
       </div>
     );
@@ -15,8 +20,8 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a comuter</h2>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </div>
     );
   }
@@ -24,22 +29,31 @@ class Header extends React.Component {
 
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
-// Options -> Options component here
+// Add Remove all button
+// setup handleRemoveAll -> alert some message
+// set onClick to fire the method
+
 class Options extends React.Component {
   render() {
     return (
       <div>
-        Options component here
-        <Option />
+        <ol>
+          {
+            this.props.options.map((option) => <Option key={option} optionText={option}/>)
+          }
+        </ol>
       </div>
     );
   }
@@ -50,7 +64,7 @@ class Option extends React.Component {
   render() {
     return (
       <div>
-        Option component here
+        {this.props.optionText}
       </div>
     )
   }
