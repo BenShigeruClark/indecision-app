9596be1a2d26cb0ee7fd7ef00e8562d1f4fd89e2
Ben <benclarkphoto@mac.com>
2019-06-24 23:55:13 -0700

    Adding component files

--- src/person.js
+++
@@ -1,9 +0,0 @@
-console.log('person.js is running');
-
-export const isAdult = (age) => age >= 18;
-
-export const canDrink = (age) => age >= 21;
-
-export default (age) => age >= 65;
-
-// export { isAdult, canDrink, isSenior as default}
